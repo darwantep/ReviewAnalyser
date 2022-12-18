bc58bc20a178e413c56339eac2bcb7959211f7c7
student <darwantep@gmail.com>
2022-12-18 14:27:31 +0000

    code

---
+++ ReviewAnalyser/src/main/java/paras/com/ReviewAnalyser/ReviewAnalyserApplication.java
@@ -0,0 +1,13 @@
+package paras.com.ReviewAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAnalyserApplication.class, args);
+	}
+
+}
---
+++ ReviewAnalyser/src/test/java/paras/com/ReviewAnalyser/ReviewAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package paras.com.ReviewAnalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
